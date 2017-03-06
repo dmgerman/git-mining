ea93f4d5e1874967ef870047bee94ebc601eebc0
D German <dmg@uvic.ca>
2017-03-06 13:52:58 +0900

    almost done

--- withCid.pl
+++ withCid.pl
@@ -5,16 +5,22 @@ my $cid;
 while (<>) {
     chomp;
     s/;/<SEMICOLON>/g;
-    if (/^commit ([0-9a-f]{40}) *(.*)$/) {
-        $cid = $1;
+    
+    if (/^commit ([0-9a-f]{40}) *(.*)$/) { 
+        # match commit id
+        $cid = $1; 
         print "$1;$2\n";
-    } elsif  (/^([0-9a-f]{40}) *(.*)$/) {
-        $cid = $1;
+    } elsif (/^([0-9a-f]{40}) *(.*)$/) {
+        # --abbrev does not include commit
+        $cid = $1; 
         print "$1;$2\n";
     } else {
-        if (/^(\D+):\s+(.*)$/) {
-           $_ = "$1;$2";
+        if (/^(\S+):\s+(.*)$/) {
+           # if it has a non-space sequence followed by :
+           # then it is a header field. split
+            print "$cid;$1;$2\n";
+        } else {
+            print "$cid;;$_\n";
         }
-        print "$cid;$_\n";
     }
 }
